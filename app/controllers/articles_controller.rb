06e946151ef9052001dc2f98f51f2a526138949e
Vitor Paiva <paiva.vb@gmail.com>
2021-04-28 22:33:59 -0300

    Show validation and successful message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   
 end
