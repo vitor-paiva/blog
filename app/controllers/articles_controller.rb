28ecda357029b85bd98b9a7b99c7ee7a1b249654
Vitor Paiva <paiva.vb@gmail.com>
2021-04-28 22:10:32 -0300

    Create new article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
+  
 end
